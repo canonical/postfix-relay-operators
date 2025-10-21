9f00d34946b6784f353134db77c4f867b2785b01
Arturo Seijas <arturo.seijas@canonical.com>
2025-10-21 15:53:07 +0200

    be careful

--- postfix-relay-operator/files/usr/local/bin/inotify_wait_postmap.sh
+++ postfix-relay-operator/files/usr/local/bin/inotify_wait_postmap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 lock=/tmp/inofity.lock
-inotifywait -mr /etc/postfix --exclude /etc/postfix/main.cf --exclude /etc/postfix/master.cf -e close_write -e create -e delete -e move --format '%w%f %e' | while read file event; do
+inotifywait -mr /etc/postfix --exclude /etc/postfix/main.cf --exclude /etc/postfix/master.cf -e close_write -e create -e delete -e move --format '%w%f %e' | while read -r file event; do
     : >> $lock #create a file if it doesn't exist
     {
     flock 3 # lock file by filedescriptor
     sleep 5
-    echo hooks/config-changed
+    /var/lib/juju/agents/$(juju_unit_agent_name)/hooks/config-changed
     } 3<$lock
 done
