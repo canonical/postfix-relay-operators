7bc63616670ef7a77e3b9083891f6baf63ba841e
Arturo Seijas <arturo.seijas@canonical.com>
2025-10-22 10:31:03 +0200

    Add terraform product with opendkim

---
+++ postfix-relay-operator/terraform-product/main.tf
@@ -0,0 +1,39 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+module "postifx_relay" {
+  source      = "../terraform"
+  app_name    = var.postifx_relay.app_name
+  channel     = var.postifx_relay.channel
+  config      = var.postifx_relay.config
+  model       = var.model
+  constraints = var.postifx_relay.constraints
+  revision    = var.postifx_relay.revision
+  base        = var.postifx_relay.base
+  units       = var.postifx_relay.units
+}
+
+module "opendkim" {
+  source      = "git::ssh://git@github.com/canonical/opendkim//terraform?depth=1&ref=rev254"
+  app_name    = var.opendkim.app_name
+  channel     = var.opendkim.channel
+  config      = var.opendkim.config
+  constraints = var.opendkim.constraints
+  model       = var.model
+  revision    = var.opendkim.revision
+  units       = var.opendkim.units
+}
+
+resource "juju_integration" "postifx_relay_opendkim" {
+  model = var.model
+
+  application {
+    name     = module.postifx_relay.app_name
+    endpoint = module.postifx_relay.requires.milter
+  }
+
+  application {
+    name     = module.opendkim.app_name
+    endpoint = module.opendkim.provides.milter
+  }
+}
---
+++ postfix-relay-operator/terraform-product/outputs.tf
@@ -0,0 +1,7 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
+  description = "Name of the deployed application."
+  value       = module.postfix_relay.app_name
+}
