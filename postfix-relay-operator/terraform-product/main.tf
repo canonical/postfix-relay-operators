0fbc5bef89a3d7feb84a7450490aa1ecb8bd7b5d
Sebastien Georget (charmkeeper) <sebastien.georget+charmkeeper@canonical.com>
2026-03-11 14:33:21 +0100

    fix(terraform): use HTTPS instead of SSH for opendkim module source

--- postfix-relay-operator/terraform-product/main.tf
+++ postfix-relay-operator/terraform-product/main.tf
@@ -14,7 +14,7 @@ module "postfix_relay" {
 }
 
 module "opendkim" {
-  source      = "git::ssh://git@github.com/canonical/opendkim//terraform?depth=1&ref=rev7"
+  source      = "git::https://github.com/canonical/opendkim//terraform?depth=1&ref=rev7"
   app_name    = var.opendkim.app_name
   channel     = var.opendkim.channel
   config      = var.opendkim.config
